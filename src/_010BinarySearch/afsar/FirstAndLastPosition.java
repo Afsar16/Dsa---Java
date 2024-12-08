cdf6b25d53f77242a66334a2e35961cdad778e34
Afsar16 <afsar.m1606@gmail.com>
2024-12-08 15:40:58 +0530

    add strings and patterns

---
+++ src/_010BinarySearch/afsar/FirstAndLastPosition.java
@@ -0,0 +1,52 @@
+package _010BinarySearch.afsar;
+
+public class FirstAndLastPosition {
+    public static void main(String[] args) {
+        int[] arr = {-18, -12, -4, 0, 2, 3, 4, 15, 16, 18, 22, 22, 45, 89};
+        int target = 22;
+        int []ans = searchRange(arr, target);
+        for (int i = 0; i < ans.length; i++) {
+            System.out.println(ans[i]);
+        }
+    }
+
+    public static int[] searchRange(int[] nums, int target) {
+
+        int[] ans = {-1, -1};
+        // check for first occurrence if target first
+        ans[0] = search(nums, target, true);
+        if (ans[0] != -1) {
+            ans[1] = search(nums, target, false);
+        }
+        return ans;
+    }
+
+    // this function just returns the index value of target
+    public static int search(int[] nums, int target, boolean findStartIndex) {
+        int ans = -1;
+        int start = 0;
+        int end = nums.length - 1;
+        while(start <= end) {
+            // find the middle element
+//            int mid = (start + end) / 2; // might be possible that (start + end) exceeds the range of int in java
+            int mid = start + (end - start) / 2;
+
+            if (target < nums[mid]) {
+                end = mid - 1;
+            } else if (target > nums[mid]) {
+                start = mid + 1;
+            } else {
+                // potential ans found
+                ans = mid;
+                if (findStartIndex) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            }
+        }
+        return ans;
+    }
+}
+
+
---
+++ src/_12strings/afsar/Main.java
@@ -0,0 +1,19 @@
+package _12strings.afsar;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] arr = {2, 3, 5, 4, 19};
+        int num = 10;
+        String name = "Kunal Kushwaha";
+//        System.out.println(name);
+
+        String a = "Kunal";
+        System.out.println(a);
+        a = "Kushwaha";
+        String b = a+ "afs";
+        System.out.println(b);
+        System.out.println(a);
+    }
+}
+
